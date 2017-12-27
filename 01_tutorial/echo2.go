0a403337aba03356c9182a2008ab6e10e631a81f
Jason Ganub <jasonganub@gmail.com>
2017-12-27 13:14:43 -0800

    Command-line argument printer using range and implicit variables.

---
+++ 01_tutorial/echo2.go
@@ -0,0 +1,16 @@
+// Prints command-line arguments using range
+package main
+
+import (
+  "fmt"
+  "os"
+)
+
+func main() {
+  s, sep := "", ""
+  for _, arg := range os.Args[1:] {
+    s += sep + arg
+    sep = " "
+  }
+  fmt.Println(s)
+}
